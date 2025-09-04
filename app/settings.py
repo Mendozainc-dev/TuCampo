7104d352c726af0019f54af1ec305b9ad70b9b47
Salvador Mendoza Munoz <Mendozainc_dev@outlook.com>
2025-09-03 23:54:06 -0600

    Creacion del formato Models para la creacion de usuaros
    Se modifico y se integro el formato de lectura para la seccion de crear usuarios, ademas se modifico el archivo settings para poder integrar correctamente la aplicacion a la bd

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'tucampo_controller',
     'app'
 ]
 
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Cors para la aplicacion (urls admitidas)
+CORS_ALLOWED_ORIGINS = [] 
--- tucampo_controller/models.py
+++ tucampo_controller/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.core.validators import RegexValidator, MinValueValidator
 # Create your models here.
+
+#clase para la creacion de un usuario tomando en cuenta datos generales como especificos
+class CreateUser(models.Model):
+    name = models.CharField(max_length=25) 
+    lastn_name = models.CharField(max_length=50)
+    birthday = models.DateField( auto_now=False, auto_now_add=False)
+    age = models.PositiveIntegerField(validators=[MinValueValidator(18)])
+    city = models.CharField(max_length=50)
+    estate = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    live_mexico = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, max_length=254)
+    number = models.CharField(
+        max_length=20,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$'
+            )
+        ]
+    )
+    charge = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    enterprise = models.CharField(max_length=30)
+    ubication = models.CharField(max_length=40)
+    
+    
+    
