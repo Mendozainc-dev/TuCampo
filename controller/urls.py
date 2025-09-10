2bc55634e90d7f2fc3ada89ce7b10be3fe739abb
Salvador Mendoza Munoz <Mendozainc_dev@outlook.com>
2025-09-09 19:13:50 -0600

    acomodo de rutas
    Acomodo de rutas

--- controller/urls.py
+++ controller/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from tucampo_controller import views
+from controller import views
 
 router = routers.DefaultRouter()
 router.register(r'create-user', views.CreateUser, basename='create-user') #La url es create-user/api/v1/create-user
